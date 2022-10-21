cdcc5a99f00227acfa812e942b1bbd4f3a32e866
张盛 <shengbox@qq.com>
2022-10-21 09:41:30 +0800

    feat:mgo

--- go.mod
+++ go.mod
@@ -3,10 +3,15 @@ module github.com/shengbox/go-util
 go 1.18
 
 require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.4.0
 	github.com/speps/go-hashids v2.0.0+incompatible
 	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
-require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+require (
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
---
+++ mgo/mgo.go
@@ -0,0 +1,126 @@
+package mgo
+
+import (
+	"log"
+	"os"
+
+	"github.com/globalsign/mgo"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var (
+	DbName  string
+	globalS *mgo.Session
+)
+
+func init() {
+	host := os.Getenv("mongodb.host")
+	if host != "" {
+		DbName = os.Getenv("mongodb.database")
+		diaInfo := &mgo.DialInfo{
+			Addrs:     []string{host},
+			Database:  "admin",
+			Username:  os.Getenv("mongodb.username"),
+			Password:  os.Getenv("mongodb.password"),
+			PoolLimit: 3,
+		}
+		//mongolog := log.New(os.Stderr, "MONGO ", log.LstdFlags)
+		//mgo.SetLogger(mongolog)
+		//mgo.SetDebug(true)
+		s, err := mgo.DialWithInfo(diaInfo)
+		if err != nil {
+			log.Fatalln("create session error", err)
+		}
+		globalS = s
+	}
+}
+
+func connect(db, collection string) (*mgo.Session, *mgo.Collection) {
+	s := globalS.Copy()
+	c := s.DB(db).C(collection)
+	return s, c
+}
+
+func Insert(db, collection string, docs ...interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Insert(docs...)
+}
+
+func FindOne(db, collection string, query, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Select(selector).One(result)
+}
+
+func FindPage(db, collection string, query, selector interface{}, pageNum, pageSize int, sort []string, result interface{}) (int, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	count, err := c.Find(query).Count()
+	if err != nil {
+		return 0, err
+	}
+	err = c.Find(query).Select(selector).Sort(sort...).Skip((pageNum - 1) * pageSize).Limit(pageSize).All(result)
+	return count, err
+}
+
+func FindAll(db, collection string, query, selector interface{}, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Select(selector).Limit(2000).All(result)
+}
+
+func FindAllSort(db, collection string, query, selector interface{}, pageNum int, result interface{}, sortFields ...string) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Select(selector).Sort(sortFields...).Skip((pageNum - 1) * 100).Limit(100).All(result)
+}
+
+func CountAll(db, collection string, query interface{}) (int, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Count()
+}
+
+func Distinct(db, collection string, query, selector interface{}, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Select(selector).Distinct("month", result)
+}
+
+func Update(db, collection string, query, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Update(query, update)
+}
+
+func UpdateAll(db, collection string, query, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	_, err := c.UpdateAll(query, update)
+	return err
+}
+
+func Upsert(db, collection string, query, update interface{}) (info *mgo.ChangeInfo, err error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Upsert(query, update)
+}
+
+func Remove(db, collection string, query interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Remove(query)
+}
+
+func RemoveAll(db, collection string, query interface{}) (*mgo.ChangeInfo, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.RemoveAll(query)
+}
+
+func Pipe(db, collection string, query interface{}, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Pipe(query).All(result)
+}
