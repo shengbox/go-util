45a273bc3ebde4fdc307560c2ac7c226e021d625
张盛 <shengbox@gmail.com>
2025-04-23 13:50:28 +0800

    fix

--- mongox/session.go
+++ mongox/session.go
@@ -12,11 +12,11 @@ import (
 // Session -
 type Session struct {
 	collection *mongo.Collection
-	filter     interface{}
+	filter     any
 	limit      int64
-	project    interface{}
+	project    any
 	skip       int64
-	sort       interface{}
+	sort       any
 }
 
 // Limit sets sorting
@@ -26,7 +26,7 @@ func (s *Session) Limit(limit int64) *Session {
 }
 
 // Project sets sorting
-func (s *Session) Project(project interface{}) *Session {
+func (s *Session) Project(project any) *Session {
 	s.project = project
 	return s
 }
@@ -38,13 +38,35 @@ func (s *Session) Skip(skip int64) *Session {
 }
 
 // Sort sets sorting
-func (s *Session) Sort(sort interface{}) *Session {
+func (s *Session) Sort(sort any) *Session {
 	s.sort = sort
 	return s
 }
 
+func (s *Session) All(result any) (err error) {
+	opts := options.Find()
+	if s.limit > 0 {
+		opts.SetLimit(s.limit)
+	}
+	if s.project != nil {
+		opts.SetProjection(s.project)
+	}
+	if s.sort != nil {
+		opts.SetSort(s.sort)
+	}
+	cur, err := s.collection.Find(context.TODO(), s.filter, opts)
+	if err != nil {
+		return err
+	}
+	defer cur.Close(context.TODO())
+	if err := cur.All(context.TODO(), result); err != nil {
+		return err
+	}
+	return nil
+}
+
 // Decode returns all docs
-func (s *Session) Page(result interface{}) (count int64, err error) {
+func (s *Session) Page(result any) (count int64, err error) {
 	count, _ = s.collection.CountDocuments(context.TODO(), s.filter)
 
 	opts := options.Find()
